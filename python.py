6468b9894f39cca29cb91eeaa0f94c3cec03f347
gomtion <terryquansah44@gmail.com>
2019-07-11 10:41:52 +0100

    first commit

---
+++ python.py
@@ -0,0 +1,19 @@
+#i = 0
+#while(i < 10):
+ #   i = i + 1
+  #  print(i)
+# i is increasing by 1 and ends at 10 
+
+#i = 6
+#while(i < 19):
+  #  i = i + 1
+   # print(i)
+    
+#i = 12
+#while(i < 18):
+ #   i = i + 2
+  #  print(i)
+  
+
+    
+
