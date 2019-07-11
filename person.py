24932418b65481a84f24e56dd354bc39c43d19a7
gomtion <terryquansah44@gmail.com>
2019-07-11 13:41:18 +0100

    first commit

--- person.py
+++ person.py
@@ -1,9 +1,3 @@
-get_age = (int(input("How old are you? ")))
-
-name = input("What is your name? ")
-
-print(f"your name is {name} and you are {get_age} years old ")
-
 def get_age(age):
    return age
 
@@ -18,8 +12,7 @@ name = input("what is your name? ")
 get_age(age)
 get_name(name)
 print(f"Your name is {name} and you are {age} years old. ")
- 
- 
 
-    
-    
+
+
+
