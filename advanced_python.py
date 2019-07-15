e61c203e754ccd5174475b2c4534b38047493fb8
gomtion <terryquansah44@gmail.com>
2019-07-15 16:51:38 +0100

    second commit

--- advanced_python.py
+++ advanced_python.py
@@ -1,11 +1,11 @@
-"""def is_even(number):
+'''def is_even(number):
     return number % 2 == 0
 numbers = [1,56,234,87,4,76,24,69,90,135]
-print(list(filter(is_even,numbers)))"""
+print(list(filter(is_even,numbers)))
 
 
-"""numbers = [1,56,234,87,4,76,24,69,90,135]
-print(list(filter(lambda x : x % 2 == 1,numbers)))"""
+numbers = [1,56,234,87,4,76,24,69,90,135]
+print(list(filter(lambda x : x % 2 == 1,numbers)))
 
 from functools import *
 
@@ -18,5 +18,16 @@ def fold(words):
         
     print(z)
      
-fold(["hello" , "world"])
+fold(["hello" , "world"])'''
     
+ # printing out a list of positive numbers from this list(numbers)   
+numbers = [34.6, -203.4, 44.9, 68.3, -12.2, 44.6, 12.7]
+positives = [n for n in numbers if n < 0]
+print(positives)
+
+# print out a list of even numbers from this list(numbers)
+numbers = [12, 54, 33, 67, 24, 89, 11, 24, 47]
+even_numbers = [n for n in numbers if n % 2 == 0]
+print(even_numbers)
+
+
