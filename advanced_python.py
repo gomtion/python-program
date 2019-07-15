f0b48e5b60ff41944d15cc27c497033685c42eb2
gomtion <terryquansah44@gmail.com>
2019-07-15 16:57:23 +0100

    third commit

--- advanced_python.py
+++ advanced_python.py
@@ -1,14 +1,14 @@
-'''def is_even(number):
+# A function to return even numbers from a list using "filter"
+def is_even(number):
     return number % 2 == 0
 numbers = [1,56,234,87,4,76,24,69,90,135]
 print(list(filter(is_even,numbers)))
 
-
+#using lamba to return odd figures from the list below
 numbers = [1,56,234,87,4,76,24,69,90,135]
 print(list(filter(lambda x : x % 2 == 1,numbers)))
 
-from functools import *
-
+#A function to add strings
 def fold(words):
     i=0
     z = ""
@@ -18,7 +18,7 @@ def fold(words):
         
     print(z)
      
-fold(["hello" , "world"])'''
+fold(["hello" , "world"])
     
  # printing out a list of positive numbers from this list(numbers)   
 numbers = [34.6, -203.4, 44.9, 68.3, -12.2, 44.6, 12.7]
